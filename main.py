f266109130bc341d8069cca0473ad871991fac70
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-04-24 14:39:07 +0500

    ля ля ля

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import render_template, Flask, url_for
+import os
 
 app = Flask(__name__)
 
@@ -51,4 +52,5 @@ def start_site():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
